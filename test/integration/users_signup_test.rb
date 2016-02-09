fce164ba8069fa9660b442933ee32d4cd16e36c5
Oliver Chen <oliver.chen@navy.mil>
2016-02-09 12:42:58 -0500

    finished login/logout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,5 +15,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: { name: "Tester Mctesterson", email: "MrTester@testerson.com", password: "password", password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,4 +63,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = "a" * 5
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
